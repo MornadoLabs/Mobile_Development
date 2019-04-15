bcfaa64b5f2bc62e08fbb11dac72b5c64f9542bc
mornado <mornado12@mail.ru>
2019-04-15 06:55:52 +0300

    Set default action.

---
+++ app/src/main/java/com/example/besteventslviv/BestEventsLvivApplication.kt
@@ -0,0 +1,9 @@
+package com.example.besteventslviv
+
+import android.app.Application
+
+class BestEventsLvivApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
